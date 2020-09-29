f7826fd19b41d9a6208ec8a03d240fd2e5c38cb8
dairlair <51401608+dairlair@users.noreply.github.com>
2020-09-29 10:53:36 +0300

    Basic test added

---
+++ mediagrabber/tests/test_mediagrabber.py
@@ -0,0 +1,13 @@
+from mediagrabber.core import MediaGrabber, FramerInterface, StorageInterface
+from mediagrabber.framer import OpencvVideoFramesRetriever
+from mediagrabber.s3 import S3Storage
+
+
+def test_constructor():
+    # When
+    framer: FramerInterface = OpencvVideoFramesRetriever('')
+    storage: StorageInterface = S3Storage('', '', '', '')
+    mg: MediaGrabber = MediaGrabber(framer, storage)
+
+    # Then
+    assert type(mg) is MediaGrabber
