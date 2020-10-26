e9a403eb5f23361920e97b8c2a1ff73d869f8959
dairlair <51401608+dairlair@users.noreply.github.com>
2020-10-26 13:12:45 +0200

    Messages processor added with test coverage (#9)

--- mediagrabber/consumer.py
+++ mediagrabber/consumer.py
@@ -1,7 +1,23 @@
 import json
+from typing import Optional
 from pika.adapters.blocking_connection import BlockingChannel
-from mediagrabber.core import MediaGrabber
+from mediagrabber.core import MediaGrabber, MediaGrabberError
 import logging
+
+
+class MessageProcessor(object):
+    def __init__(self, service: MediaGrabber, callback):
+        self.service = service
+        self.callback = callback
+
+    # Returns dict when message is processed (successfully or with errors).
+    def process(self, payload: dict) -> Optional[dict]:
+        try:
+            response = self.callback(self.service, payload)
+            return {**payload, **response, "success": True}
+        except MediaGrabberError as err:
+            logging.error(err)
+            return {**payload, "success": False, "error": err.__dict__}
 
 
 class Consumer(object):
@@ -13,22 +29,20 @@ class Consumer(object):
         queue_out: str,
         callback,
     ):
-        self.service = service
         self.channel = channel
-        self.callback = callback
         self.queue_out = queue_out
         self.channel.queue_declare(queue_in, durable=True)
         self.channel.queue_declare(queue_out, durable=True)
         self.channel.basic_consume(queue_in, self.on_message)
+        self.processor = MessageProcessor(service, callback)
 
     def on_message(self, ch: BlockingChannel, method, properties, body: str):
         logging.info(f"Incoming message received: {body}")
+
         payload = json.loads(body)
-        response = self.callback(self.service, payload)
-        if response is None:
-            payload = {**payload, "success": False}
-        else:
-            payload = {**payload, **response, "success": True}
+        payload = self.processor.process(payload)
         body = json.dumps(payload)
-        self.channel.basic_publish(exchange="", routing_key=self.queue_out, body=body)
+
+        logging.info(f"Outcoming message prepared: {body}")
+        self.channel.basic_publish("", self.queue_out, body)
         ch.basic_ack(delivery_tag=method.delivery_tag)
