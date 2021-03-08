46e5f7bb7772de7123c11a156cb0f53a6328cb13
dairlair <51401608+dairlair@users.noreply.github.com>
2021-03-09 01:01:10 +0200

    Few investigations

---
+++ dec.py
@@ -0,0 +1,41 @@
+from decord import VideoReader, cpu
+from console_progressbar import ProgressBar
+
+vr = VideoReader('workdir2/Constantine.2005.HD-DVDRip.1080p.HEVC.10bit.mkv', ctx=cpu(0))
+length = len(vr)
+print('video frames:', length)
+frames_numbers = range(0, length, 150)
+frames = vr.get_batch(frames_numbers)
+
+# # a file like object works as well, for in-memory decoding
+# with open('workdir2/Constantine.2005.HD-DVDRip.1080p.HEVC.10bit.mkv', 'rb') as f:
+#     print('opened')
+#     vr = VideoReader(f, ctx=cpu(0), num_threads=1)
+#     print('video frames:', len(vr))
+#     # pb = ProgressBar(total=vr, prefix='Here', suffix='Now', decimals=3, length=50, fill='X', zfill='-')
+#     # # frames = range(0, len(vr), 150)
+#     # # 1. the simplest way is to directly access frames
+#     # for i in range(len(vr)):
+#     #     # the video reader will handle seeking and skipping in the most efficient manner
+#     #     frame = vr[i]
+#     #     pb.print_progress_bar(i)
+#     #     # print(frame.shape)
+
+# # To get multiple frames at once, use get_batch
+# # this is the efficient way to obtain a long list of frames
+# frames = vr.get_batch([1, 3, 5, 7, 9])
+# print(frames.shape)
+# # (5, 240, 320, 3)
+# # duplicate frame indices will be accepted and handled internally to avoid duplicate decoding
+# frames2 = vr.get_batch([1, 2, 3, 2, 3, 4, 3, 4, 5]).asnumpy()
+# print(frames2.shape)
+# # (9, 240, 320, 3)
+
+# # 2. you can do cv2 style reading as well
+# # skip 100 frames
+# vr.skip_frames(100)
+# # seek to start
+# vr.seek(0)
+# batch = vr.next()
+# print('frame shape:', batch.shape)
+# print('numpy frames:', batch.asnumpy())
--- test.py
+++ test.py
@@ -1,39 +1,104 @@
 import face_recognition
 import cv2
+from PIL import Image, ImageDraw
+from console_progressbar import ProgressBar
+from time import time_ns
+
+
+# Download video for test
+# youtube-dl -f "bestvideo[height<=480]+bestaudio/best[height<=480]" https://www.youtube.com/watch?v=ozqhwa9K0gk -o "./workdir/source.%(ext)s"
 
 # Open the input movie file
-input_movie = cv2.VideoCapture("workdir/1f9108b10e771890eeec0d0fa1a67fd2/source.mp4")
+# input_movie = cv2.VideoCapture("workdir/source.mp4")
+input_movie = cv2.VideoCapture("workdir2/Constantine.2005.HD-DVDRip.1080p.HEVC.10bit.mkv")
+
 
 length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
 print(f'Frames count: {length}')
 
-seconds = 5
 fps = int(round(input_movie.get(cv2.CAP_PROP_FPS)))  # Gets the frames per second
-multiplier = fps * seconds
 print(f'FPS: {fps}')
+
+pb = ProgressBar(total=length, prefix='Here', suffix='Now', decimals=3, length=50, fill='X', zfill='-')
+
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
 frame_number = 0
 
+known_face_encodings = []
+
+def is_known(face_encoding) -> bool:
+    global known_face_encodings
+    results = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    if True in results:
+        return True
+
+    known_face_encodings.append(face_encoding)
+    return False
+
+
 while input_movie.isOpened():
     ret, frame = input_movie.read()
 
     if ret:
+        pb.print_progress_bar(frame_number)
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_frame = frame[:, :, ::-1]
 
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_frame)
+        face_locations = face_recognition.face_locations(rgb_frame, 1, "fog")
         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
         if len(face_encodings):
-            # cv2.imshow('Frame with faces', frame)
-            cv2.imwrite(f'workdir/face-{frame_number}.png', frame)
-            print(f"Faces found in the frame #{frame_number}")
+            pil_image = Image.fromarray(rgb_frame)
+            draw = ImageDraw.Draw(pil_image)
 
-        frame_number += fps * 2  # i.e. at 30 fps, this advances one second
+            # Loop through each face found in the unknown image
+            face_number = 0
+            for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+                # See if the face is a match for the known face(s)
+                # matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+                # name = "Unknown"
+
+                # # If a match was found in known_face_encodings, just use the first one.
+                # # if True in matches:
+                # #     first_match_index = matches.index(True)
+                # #     name = known_face_names[first_match_index]
+
+                # # Or instead, use the known face with the smallest distance to the new face
+                # face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                # best_match_index = np.argmin(face_distances)
+                # if matches[best_match_index]:
+                #     name = known_face_names[best_match_index]
+
+                # Draw a box around the face using the Pillow module
+                start = time_ns()
+                if is_known(face_encoding):
+                    outline = (255, 0, 0)
+                else:
+                    outline = (0, 0, 255)
+                    face = pil_image.crop(box=(left, top, right, bottom))
+                    face.save(f'workdir2/faces/face-{frame_number}-{face_number}.png')
+                    face_number += 1
+                    draw.rectangle(((left, top), (right, bottom)), outline=outline)
+                    pil_image.save(f'workdir2/frame-{frame_number}.png')
+                finish = time_ns()
+
+                print("Matching duration: " + str(finish - start) + " nanosecods")
+
+            # if len(face_encodings):
+            #     pil_image = Image.fromarray(rgb_frame)
+            #     print(f"Faces found in the frame #{frame_number}")
+            #     for j, face_encoding in enumerate(face_encodings):
+            #         if (is_known(face_encoding)):
+            #             print("This is a new face!")
+            #             # cv2.imshow('Frame with faces', frame)
+            #             cv2.imwrite(f'workdir/face-{frame_number}-{j}.png', frame)
+
+        frame_number += fps * 10  # i.e. at 30 fps, this advances two secods
         frame_number = int(round(frame_number))
         input_movie.set(1, frame_number)
     else:
