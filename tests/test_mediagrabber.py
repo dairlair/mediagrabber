12e7f971b6eaa320f516dd3c9fc74421004a42e6
dairlair <51401608+dairlair@users.noreply.github.com>
2021-04-27 12:12:12 +0300

    Tests fixed

--- tests/test_mediagrabber.py
+++ tests/test_mediagrabber.py
@@ -1,19 +1,25 @@
 from mediagrabber.core import MediaGrabber
-from mediagrabber.downloader.youtubedl import YoutubedlVideoDownloader
-from mediagrabber.retriever.av import AvFramesRetriever
+from mediagrabber.downloader.factory import MediaDownloaderFactory
+from mediagrabber.retriever.factory import FramesRetrieverFactory
 from mediagrabber.resizer.default import DefaultFramesResizer
 from mediagrabber.detector.unique import UniqueFaceDetector
 from mediagrabber.publisher.file import FileFacePublisher
+from mediagrabber.distancer.annoy import AnnoyDistancer
+from mediagrabber.storage.postgres import PostgreSQLStorage
 
 
 def test_constructor():
     # When
-    downloader = YoutubedlVideoDownloader('')
-    retriever = AvFramesRetriever()
+    retriever_factory = FramesRetrieverFactory()
     resizer = DefaultFramesResizer()
     detector = UniqueFaceDetector()
     publisher = FileFacePublisher()
-    mg: MediaGrabber = MediaGrabber(downloader, retriever, resizer, detector, publisher)
+    downloader_factory = MediaDownloaderFactory("")
+    storage = PostgreSQLStorage("postgresql://localhost:5432/db")
+    distancer = AnnoyDistancer(storage)
+    mg: MediaGrabber = MediaGrabber(
+        retriever_factory, resizer, detector, publisher, storage, downloader_factory, distancer
+    )
 
     # Then
     assert type(mg) is MediaGrabber
