3019c8f5a8e035e917334ca9f87fe83a2282c940
dairlair <51401608+dairlair@users.noreply.github.com>
2021-05-06 23:15:30 +0300

    The first test added

---
+++ tests/test_memorize.py
@@ -0,0 +1,29 @@
+import pytest
+from typing import List
+from injector import Injector
+from mediagrabber.dependencies import configure
+from mediagrabber.core import MediaGrabber
+
+
+@pytest.fixture
+def setup() -> MediaGrabber:
+    injector = Injector([configure])
+    service: MediaGrabber = injector.get(MediaGrabber)
+    yield service
+
+
+@pytest.fixture
+def urls() -> List[str]:
+    return [
+        "https://images.unsplash.com/photo-1517486808906-6ca8b3f04846?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=100",
+    ]
+
+
+class TestMemorizingFromPhotos:
+    def test_faces_memorizing_from_photos(self, setup, urls):
+        mg: MediaGrabber = setup
+        for url in urls:
+            result = mg.memorize(url, "direct")
+            assert isinstance(result, List), "The `memorize` response must be a list"
+            for piece in result:
+                assert 'faces' in piece, "Each piece of response must contains faces attributes"
