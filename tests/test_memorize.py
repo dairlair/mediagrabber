f3859db04246e36581149ad481c4e200f5880d7f
dairlair <51401608+dairlair@users.noreply.github.com>
2021-05-11 09:27:32 +0300

    Memorizing from video url added

--- tests/test_memorize.py
+++ tests/test_memorize.py
@@ -13,17 +13,40 @@ def mg() -> MediaGrabber:
 
 
 @pytest.fixture
-def urls() -> List[str]:
+def photos_urls() -> List[str]:
     return [
         "https://images.unsplash.com/photo-1517486808906-6ca8b3f04846?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=100",
     ]
 
 
+@pytest.fixture
+def photos_files() -> List[str]:
+    return [
+        "tests/data/photos/5faces.jpg",
+    ]
+
+
+@pytest.fixture
+def video_urls() -> List[str]:
+    return [
+        "https://abcnews.go.com/Technology/video/california-judge-orders-uber-lyft-reclassify-drivers-employees-72302309",
+    ]
+
+
 class TestMemorizingFromPhotos:
-    def test_faces_memorizing_from_photos(self, mg, urls):
+    def test_memorizing_from_photos_urls(self, mg, photos_urls):
+        self.memorizing(mg, photos_urls, "direct")
+
+    def test_memorizing_from_photos_files(self, mg, photos_files):
+        self.memorizing(mg, photos_files, "direct")
+
+    def test_memorizing_from_video_urls(self, mg, video_urls):
+        self.memorizing(mg, video_urls, "youtubedl")
+
+    def memorizing(self, mg, addresses, source="youtubedl"):
         mg: MediaGrabber = mg
-        for url in urls:
-            result = mg.memorize(url, "direct")
+        for file in addresses:
+            result = mg.memorize(file, source)
             assert isinstance(result, List), "The `memorize` response must be a list"
             for piece in result:
                 assert "faces" in piece, "Each piece of response must contains faces attributes"
